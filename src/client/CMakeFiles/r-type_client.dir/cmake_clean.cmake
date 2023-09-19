3b59bdf1991edfbeb38bcba028bd726da29ebef1
Yom <tom.laiolo@epitech.eu>
2023-09-19 18:11:50 +0200

    Add basic server and client

---
+++ src/client/CMakeFiles/r-type_client.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../../r-type_client"
+  "../../r-type_client.pdb"
+  "CMakeFiles/r-type_client.dir/Args.cpp.o"
+  "CMakeFiles/r-type_client.dir/Args.cpp.o.d"
+  "CMakeFiles/r-type_client.dir/Client.cpp.o"
+  "CMakeFiles/r-type_client.dir/Client.cpp.o.d"
+  "CMakeFiles/r-type_client.dir/main.cpp.o"
+  "CMakeFiles/r-type_client.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/r-type_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/client/build/CMakeFiles/r-type_client.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/chris/delivery/RTYPE/RTypeMirror/src/client/Args.cpp" "CMakeFiles/r-type_client.dir/Args.cpp.o" "gcc" "CMakeFiles/r-type_client.dir/Args.cpp.o.d"
+  "/home/chris/delivery/RTYPE/RTypeMirror/src/client/Client.cpp" "CMakeFiles/r-type_client.dir/Client.cpp.o" "gcc" "CMakeFiles/r-type_client.dir/Client.cpp.o.d"
+  "/home/chris/delivery/RTYPE/RTypeMirror/src/client/main.cpp" "CMakeFiles/r-type_client.dir/main.cpp.o" "gcc" "CMakeFiles/r-type_client.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
