d97b44099bef3bfc368bc523acfd0ec20ff0d4c9
login <login@botdesign.net>
2023-10-03 11:56:16 +0200

    Add: graph display class

--- src/ECS/Graph.hpp
+++ src/ECS/Graph.hpp
@@ -11,6 +11,7 @@
 #include <tuple>
 #include <optional>
 #include "Enum.hpp"
+#include "Entity.hpp"
 
 class Graph {
     public:
@@ -18,9 +19,11 @@ class Graph {
         ~Graph();
 
         std::vector<Input> getInputs() const;
-        int displayEntities(std::vector<EntityId> entities);
+        int displayEntities(std::vector<ECS::Entity> entities);
 
     protected:
     private:
+        int displayTexture(ECS::Entity &entity);
+        int displayText(ECS::Entity &entity);
         std::vector<Input> _events;
 };
