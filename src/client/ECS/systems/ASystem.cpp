e3b5cb0ea0a2328860f8205f4d1f8bd50a5de50b
StEgo2103 <lucadeltort23@gmail.com>
2023-10-04 15:17:27 +0200

    Fix: compil with cmake

--- src/client/ECS/systems/ASystem.cpp
+++ src/client/ECS/systems/ASystem.cpp
@@ -16,12 +16,27 @@ namespace ECS {
 
     void ASystem::setEntity(Entity entity)
     {
+<<<<<<< HEAD
         _entity = entity;
+=======
+        for (auto &entity : listEntities) {
+            if (entity.id.second > _listEntities.size()) {
+                _listEntities.resize(entity.id.second + 1);
+            }
+            _listEntities[entity.id.second] = entity;
+        }
+>>>>>>> 0fbc961 (Fix: compil with cmake)
     }
 
     void ASystem::removeEntity()
     {
+<<<<<<< HEAD
         _entity = std::nullopt;
+=======
+        for (auto &entity : listEntities) {
+            _listEntities[entity.id.second] = std::nullopt;
+        }
+>>>>>>> 0fbc961 (Fix: compil with cmake)
     }
 
     std::optional<Entity> ASystem::getEntity() const
