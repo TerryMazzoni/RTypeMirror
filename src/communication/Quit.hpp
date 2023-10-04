e788fa71b5b2a8075e8dbf39625183ecc6b31954
Yom <tom.laiolo@epitech.eu>
2023-10-04 14:34:19 +0200

    Add enum communication and quit struct

---
+++ src/communication/Quit.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** RTypeMirror
+** File description:
+** Quit
+*/
+
+#pragma once
+
+#include "Communication.hpp"
+
+typedef struct Quit {
+        Communication code = Communication::QUIT;
+} Quit;
