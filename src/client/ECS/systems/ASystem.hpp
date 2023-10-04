ecafa08e6e5786464092ade01dc50bb03651082c
StEgo2103 <lucadeltort23@gmail.com>
2023-10-04 15:17:27 +0200

    Fix: compil with cmake

--- src/client/ECS/systems/ASystem.hpp
+++ src/client/ECS/systems/ASystem.hpp
@@ -14,6 +14,7 @@
 namespace ECS {
     class ASystem : public ISystem {
         public:
+<<<<<<< HEAD
             ~ASystem() = default;
 
             /**
@@ -50,5 +51,18 @@ namespace ECS {
         protected:
             bool _isActivated = true;
             std::optional<Entity> _entity;
+=======
+            ASystem() = default;
+            ~ASystem() = default;
+
+            virtual std::vector<Action> execute() = 0;
+            void setActivation(bool state);
+            void addEntities(std::vector<Entity> listEntities);
+            void removeEntities(std::vector<Entity> listEntities);
+
+        protected:
+            bool _isActivated = true;
+            std::vector<std::optional<Entity>> _listEntities;
+>>>>>>> 0fbc961 (Fix: compil with cmake)
     };
 } // namespace ECS
