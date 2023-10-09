36cdc204d2eac6d6ebf8106557c276448d976c53
Yom <tom.laiolo@epitech.eu>
2023-10-09 14:59:02 +0200

    Delete index.cpp

--- src/index.cpp
+++
@@ -1,8 +0,0 @@
-/**
- * @mainpage Main page of the project
- *
- * Welcome to the main page of the project. Here you can find the documentation of the project.
- * The project is our own RTYPE game.
- *
- * @image html assets/rtype.png "Rtype logo" width=200px
- */
