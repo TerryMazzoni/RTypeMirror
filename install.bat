9ac3b26988c974c1ba17e273f74dbdc041df55d4
Tyki Mikk <christopher.artigas-fuentes@epitech.eu>
2023-10-10 14:51:58 +0200

    Add Windows .exe bat file.

---
+++ install.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Building r-type_server and r-type_client...
+
+set BUILD_DIR=build
+
+for /f %%a in ('"wmic cpu get NumberOfCores | findstr [0-9]"') do set NUM_CORES=%%a
+
+cmake -B %BUILD_DIR%
+cmake --build %BUILD_DIR% --config Release --parallel %NUM_CORES%
+
+echo Build completed. .exe files are in the Release directory.
+pause
