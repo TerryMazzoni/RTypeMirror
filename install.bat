c9132b33bdf82894abd7b335af790fb70a8623d7
AntoninLaudon <antonin.laudon@epitech.eu>
2023-10-25 13:52:07 +0200

    Add: pause in install.bat

--- install.bat
+++ install.bat
@@ -8,6 +8,7 @@ for /f %%a in ('"wmic cpu get NumberOfCores | findstr [0-9]"') do set NUM_CORES=
 cmake -B %BUILD_DIR%
 cmake --build %BUILD_DIR% --config Release --parallel %NUM_CORES%
 
-echo Build completed. .exe files are in the Release directory.
 copy Release\r-type_server.exe .
 copy Release\r-type_client.exe .
+echo Build completed. .exe files are in the Release directory.
+pause
