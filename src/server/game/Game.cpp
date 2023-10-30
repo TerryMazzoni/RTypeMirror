62ad080475d7e32ee4e5650c0e79cdf22e522d17
ArtigasChristopher <christopher.artigas-fuentes@epitech.eu>
2023-10-30 15:10:48 +0100

    Fix bullets, add teams to bullets, enemies and players.

--- src/server/game/Game.cpp
+++ src/server/game/Game.cpp
@@ -99,6 +99,8 @@ void Game::initGame(std::string map_path)
                 Parser::setValue(entity.value().instance, "team", 1);
             }
         }
+        if (entity.type == "__player__")
+            Parser::setValue(entity.instance, "team", 0);
     }
 }
 
