b174f8885f36bdbce7d402669d527e8ee33f12b6
ArtigasChristopher <ArtigasChristopher@users.noreply.github.com>
2023-10-31 17:58:53 +0000

    deploy: e9f459d21e2a2b64f06c6f245466c56bd32f6d9d

--- search/functions_9.js
+++ search/functions_9.js
@@ -3,7 +3,7 @@ var searchData=
   ['receiveasync_295',['receiveAsync',['../classClient.html#a928d5fe05f984463335ccb086d6863ca',1,'Client::receiveAsync()'],['../classServer.html#a9b1f96c79053db2877539db608ade9ac',1,'Server::receiveAsync()']]],
   ['removeallclients_296',['removeAllClients',['../classServer.html#ac12858f31a86f891256969b4985e6b16',1,'Server']]],
   ['removeclient_297',['removeClient',['../classServer.html#a050309497dab628564e509abf8b6514b',1,'Server']]],
-  ['removeentities_298',['removeEntities',['../classECS_1_1EntitiesManager.html#a52877cc4916ae17af34f3d5b7a6f4eea',1,'ECS::EntitiesManager']]],
+  ['removeentities_298',['removeEntities',['../classECS_1_1EntitiesManager.html#a52877cc4916ae17af34f3d5b7a6f4eea',1,'ECS::EntitiesManager::removeEntities(std::vector&lt; Entity &gt; entitiesToRemove)'],['../classECS_1_1EntitiesManager.html#af80814be6d6ede54749e4b6114eb787b',1,'ECS::EntitiesManager::removeEntities(std::vector&lt; int &gt; entitiesToRemove)']]],
   ['removeentity_299',['removeEntity',['../classECS_1_1ASystem.html#ad4978aa0518d8b14bb3b2db7ed5ac518',1,'ECS::ASystem::removeEntity()'],['../classECS_1_1ISystem.html#a77b3ad223bcb3511bde2dd654d199f9f',1,'ECS::ISystem::removeEntity()']]],
   ['removeinputat_300',['removeInputAt',['../classServer.html#af5f9e1e8177f98f4cbdc78c35e8bd2b8',1,'Server']]],
   ['removesystems_301',['removeSystems',['../classECS_1_1SystemManager.html#a49287efc630a343b68de5fc56a7fca0e',1,'ECS::SystemManager']]],
